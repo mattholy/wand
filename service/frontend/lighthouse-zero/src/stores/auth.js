99fd572638bbfe88f77d4f77f01a7a5d49dddee9
马特浩倪 <smile.used@hotmail.com>
2024-01-10 00:47:16 +0800

    一些测试的工作

--- service/frontend/lighthouse-zero/src/stores/auth.js
+++ service/frontend/lighthouse-zero/src/stores/auth.js
@@ -3,16 +3,18 @@ import { defineStore } from 'pinia';
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    isLoggedIn: false
+    isLoggedIn: localStorage.getItem('isLoggedIn') === 'true'
   }),
   actions: {
     login() {
       // 实际的登录逻辑
-      this.isLoggedIn = true;
+      this.isLoggedIn = true
+      localStorage.setItem('isLoggedIn', 'true')
     },
     logout() {
       // 实际的登出逻辑
-      this.isLoggedIn = false;
+      this.isLoggedIn = false
+      localStorage.setItem('isLoggedIn', 'false')
     }
   }
 });
